e4672d7f267b6db56791d9c6f8f23f8f6a10e4cf
follower <follower@rancidbacon.com>
2018-02-03 20:23:31 +1300

    Change to use the C++ API STL export so we can use a single-thread.
    We use a single thread for future compatibility with WebAssembly.
    
    Also add constant for resolution and add documentation.

--- libfive/examples/example-cxx-libfive-to-stl.cpp
+++ libfive/examples/example-cxx-libfive-to-stl.cpp
@@ -12,8 +12,12 @@
 #include <iterator>
 
 #include "libfive.h"
+#include "libfive/solve/bounds.hpp"
+#include "libfive/render/brep/mesh.hpp"
+
 
 const char *OUTPUT_FILENAME = "exported.stl";
+const float OUTPUT_RESOLUTION = 15.0;
 
 int main() {
 
@@ -29,7 +33,15 @@ int main() {
 
   std::cout << "Tree: " << libfive_tree_print(&out) << "\n";
 
-  libfive_tree_save_mesh(&out, libfive_tree_bounds(&out), 15, OUTPUT_FILENAME);
+  // We could use this C API function to export the STL but it is not
+  // possible to set `multithread` to false with this approach.
+  //
+  // libfive_tree_save_mesh(&out, libfive_tree_bounds(&out), OUTPUT_RESOLUTION, OUTPUT_FILENAME);
+
+  // Use the C++ API to export STL file in a single-threaded manner (by setting `multithread` to false).
+  //
+  // The value for `max_err` is cargo-culted from its default value.
+  Kernel::Mesh::render(out, findBounds(out), 1.0/OUTPUT_RESOLUTION, 1e-8, false)->saveSTL(OUTPUT_FILENAME);
 
   std::cout << "Exported file: " << OUTPUT_FILENAME << "\n";
 
