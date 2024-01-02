115e0bc49901249d1ecc3a8b5dcec3e2d19ff811
Ammar Hattab <Ammarkhat@gmail.com>
2024-01-02 17:44:47 -0500

    A few fixes

--- libfive/api/libfive-api.cpp
+++ libfive/api/libfive-api.cpp
@@ -4,21 +4,30 @@
 // #include <iterator>
 
 #include "libfive.h"
-#include "libfive/solve/bounds.hpp"
 #include "libfive/render/brep/mesh.hpp"
+#include "libfive/tree/tree.hpp"
+#include "../stdlib/stdlib_impl.hpp"
+#include "libfive/render/brep/settings.hpp"
 
+// Tree _union(Tree, Tree);
+// Tree intersection(Tree, Tree);
+// Tree inverse(Tree);
+// Tree difference(Tree, Tree);
+// Tree offset(Tree, TreeFloat);
+// Tree sphere(TreeFloat, TreeVec3);
 
 const char *OUTPUT_FILENAME = "exported.stl";
 const float OUTPUT_RESOLUTION = 15.0;
 
 using namespace emscripten;
+using namespace libfive;
 
-Kernel::Tree sphere(float radius, float cx, float cy, float cz){
-  auto x = Kernel::Tree::X() - Kernel::Tree(cx);
-  auto y = Kernel::Tree::Y() - Kernel::Tree(cy);
-  auto z = Kernel::Tree::Z() - Kernel::Tree(cz);
+Tree sphere(float radius, float cx, float cy, float cz){
+  auto x = Tree::X() - Tree(cx);
+  auto y = Tree::Y() - Tree(cy);
+  auto z = Tree::Z() - Tree(cz);
 
-  auto r = Kernel::Tree(radius);
+  auto r = Tree(radius);
   auto out = (x * x) + (y * y) + (z * z) - r;
   return out;
 }
@@ -37,7 +46,17 @@ void meshImplicitFunction(std::string implicitString) {
   // std::cout << "Tree: " << libfive_tree_print(&out) << "\n";
 
   // The value for `max_err` is cargo-culted from its default value.
-  Kernel::Mesh::render(out, findBounds(out), 1.0/OUTPUT_RESOLUTION, 1e-8, false)->saveSTL(OUTPUT_FILENAME);
+  // Pick the target region to render
+  auto bounds = Region<3>({-5, -5, -5}, {5, 5, 5});
+
+  // Mesh::render returns a unique_ptr, so it cleans up automatically
+  BRepSettings settings;
+  settings.min_feature = 1.0/OUTPUT_RESOLUTION;
+  settings.max_err = 1e-8;
+  settings.workers = 1;
+
+  // Mesh::render(out, bounds, 1.0/OUTPUT_RESOLUTION, 1e-8, false)->saveSTL(OUTPUT_FILENAME);
+  Mesh::render(out, bounds, settings)->saveSTL(OUTPUT_FILENAME);
 
   // std::cout << "Exported file: " << OUTPUT_FILENAME << "\n";
 
